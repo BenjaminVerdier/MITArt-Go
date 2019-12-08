b272b7bdf79585a6a3288fde1d4e4f549772686d
Benjamin Verdier <ben.verdier@hotmail.fr>
2019-12-08 12:37:52 -0500

    updated collection view

--- app/src/main/java/com/example/mitartgo/CollectionActivity.kt
+++ app/src/main/java/com/example/mitartgo/CollectionActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mitartgo
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Gravity.*
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Space
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_art_detail.*
@@ -19,6 +24,11 @@ class CollectionActivity : AppCompatActivity() {
 
         supportActionBar!!.title = "Collection"
 
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        var width = displayMetrics.widthPixels
+
         for (artpiece in MapsActivity.userCol) {
             val lay = LinearLayout(this)
             val img = ImageView(this)
@@ -30,11 +40,29 @@ class CollectionActivity : AppCompatActivity() {
             img.setImageBitmap(b)
 
             lay.addView(img)
+
             val txtView = TextView(this)
-            val txt = artpiece["title"] + ", " + artpiece["artist"]+ "," + artpiece["date"]
+            val txt = artpiece["title"] + ", " + artpiece["artist"]+ ", " + artpiece["date"]
             txtView.text = txt
+            txtView.height = 250
+            txtView.width = width - 250
+            txtView.gravity = CENTER
             lay.addView(txtView)
+
+            lay.isClickable = true
+
+            lay.setOnClickListener {
+                val intent = Intent(this, ArtDetailActivity::class.java)
+                    intent.putExtra("title", artpiece["title"])
+                    intent.putExtra("image", artpiece["image"])
+                    intent.putExtra("description", artpiece["description"])
+                    intent.putExtra("artist", artpiece["artist"])
+                    intent.putExtra("date", artpiece["date"])
+                startActivity(intent)
+            }
+
             linLayoutCol.addView(lay)
+
         }
     }
 }
