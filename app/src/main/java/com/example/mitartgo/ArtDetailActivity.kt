298903b4d267d9036828ecb3b7bdd7723e8c86fa
Benjamin Verdier <ben.verdier@hotmail.fr>
2019-12-07 21:05:54 -0500

    Working icons and detail page

--- app/src/main/java/com/example/mitartgo/ArtDetailActivity.kt
+++ app/src/main/java/com/example/mitartgo/ArtDetailActivity.kt
@@ -2,18 +2,34 @@ package com.example.mitartgo
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_art_detail.*
 
 class ArtDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_art_detail)
-    }
+        val title = intent.getStringExtra("title")
+        val image = intent.getStringExtra("image")
+        val description = intent.getStringExtra("description")
 
-    fun newIntent(context: Context): Intent {
-        val intent = Intent(context, ArtDetailActivity::class.java)
-        return intent
+        Log.d("dbg", title!!)
+        Log.d("dbg", description!!)
+        Log.d("dbg", image!!)
+
+        textView.text = title
+        textView2.text = description
+
+
+        var bMap : Bitmap? = null
+        while (bMap == null) {
+            bMap = getBitmapFromURL(image)
+        }
+
+        imageView.setImageBitmap(bMap)
     }
 }
